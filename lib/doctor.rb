bb8a1cbec4a8ff1c090bd2d885e75245e974c707
Mehmet <mehmetkusken@gmail.com>
2021-03-11 22:26:40 -0700

    commit tree 0161921661bacd49ab0daf66a9bd73421de18bcd

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
+class Doctor
+  @@all = []
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    new = Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    self.appointments.map do |appointment|
+      appointment.patient
+    end
+  end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,30 @@
+class Patient
+  attr_reader :name, :doctor, :date
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
+  def new_appointment(doctor, date)
+    Appointment.new(date,self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def doctors
+    self.appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+
+
+end
